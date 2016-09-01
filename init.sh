d5c4015cb5ac114b2e472a819eb3ad52aacf98ba
den <den@mail.ru>
2016-09-01 22:10:53 +0500

    first commit

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo ln -s /home/den/django/box/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo service nginx restart
+#sudo ln -s /home/den/django/box/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
